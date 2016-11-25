0cad5e0ab9b98a79a8ee4347b84e417254b3c53e
Chris Fitzpatrick <chrisfitzpat@gmail.com>
2016-11-25 10:31:37 +0100

    LIBITD-617 Add more promoted skills
    This adds some more promoted skills to the skills selection
    
    https://issues.umd.edu/browse/LIBITD-617

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,3 +4,7 @@ Skill.find_or_create_by(name: 'Use of Office Machinery (i.e. fax, copier )', pro
 Skill.find_or_create_by(name: 'Clerical Duties', promoted: true)
 Skill.find_or_create_by(name: 'Foreign Languages', promoted: true)
 Skill.find_or_create_by(name: 'Previous Library Experience', promoted: true)
+Skill.find_or_create_by(name: 'Mathematics and Numeracy', promoted: true)
+Skill.find_or_create_by(name: 'Creative', promoted: true)
+Skill.find_or_create_by(name: 'Social Media', promoted: true)
+Skill.find_or_create_by(name: 'Communication', promoted: true)
