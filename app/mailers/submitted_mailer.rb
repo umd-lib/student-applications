a79e2ae8e3dd88eee3f19fcf14fabc41791df57a
David P. Steelman <dsteelma@umd.edu>
2017-01-04 08:39:20 -0500

    LIBITD-666. Added logging to indicate mail server on sent emails
    https://issues.umd.edu/browse/LIBITD-666

--- app/mailers/submitted_mailer.rb
+++ app/mailers/submitted_mailer.rb
@@ -1,7 +1,11 @@
+# Mailer for successful application submissions
 class SubmittedMailer < ApplicationMailer
   default from: 'no_reply@umd.edu'
 
   def default_email(prospect)
+    logger.info('SubmittedMailer#default_email: Mail server=' +
+                smtp_settings['address'.to_sym] +
+                ", port=#{smtp_settings['port'.to_sym]}")
     @prospect = prospect
     mail(to: @prospect.email, subject: 'We appreciate your interest in employment at UMD Libraries!')
   end
