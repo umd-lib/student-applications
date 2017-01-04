d0aa35122d2cdc51ca4c5d0c9442889f7d4a6c8a
David P. Steelman <dsteelma@umd.edu>
2017-01-04 08:38:32 -0500

    LIBITD-666. Added "env_example" file with SMTP properties
    The "env_example" is copied to ".env", then filled out with the
    appropriate values.
    
    Updated the "development.rb" and "production.rb" files in
    config/environments/ to use the SMTP properties.
    
    https://issues.umd.edu/browse/LIBITD-666

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,14 +77,16 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
+  # Sets SMTP mailing options
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
-    port: 587,
-    address: 'smtp.mailgun.org',
-    domain: ENV['domain'],
-    user_name: ENV['username'],
-    password: ENV['password'],
-    authentication: :plain
-
+    address: ENV['SMTP_ADDRESS'],
+    port: ENV['SMTP_PORT'],
+    domain: ENV['SMTP_DOMAIN'],
+    user_name: ENV['SMTP_USER_NAME'],
+    password: ENV['SMTP_PASSWORD'],
+    authentication: ENV['SMTP_AUTHENTICATION'].present? ? ENV['SMTP_AUTHENTICATION'].to_sym : nil
   }
+  # Only keep non-nil values
+  config.action_mailer.smtp_settings.select! { |_k, v| v }
 end
