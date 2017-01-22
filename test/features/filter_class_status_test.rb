2a0433da9e1d35fdb413c5296049445c3e75b233
Chris Fitzpatrick <chrisfitzpat@gmail.com>
2017-01-22 15:00:40 +0100

    LIBITD-723 Add additional logging for submission failures
    This catches and submission failures on save attempts ( either via ActiveRecord
    issues or ActionMailer problems ) and adds some additional logging.
    
    Also some minor tweaks to some capybara tests to tamp down on itermittent
    failures due to AJAX.
    
    https://issues.umd.edu/browse/LIBITD-723

--- test/features/filter_class_status_test.rb
+++ test/features/filter_class_status_test.rb
@@ -2,34 +2,36 @@ require 'test_helper'
 
 feature 'Should be able filter prospects on class status' do
   scenario 'login as user & filter', js: true do
-    page.driver.resize_window(2048, 2048)
+    page.driver.resize_window(2048, 9999)
 
     undergrad = Enumeration.find_by( value: "Undergraduate" ).id
-    
-
     User.create(cas_directory_id: 'filterer', name: 'filterer', admin: false)
+    
+    students = Prospect.all.group_by(&:first_name).map { |k,v| [k, v.first] }.to_h
+
     visit prospects_path
     fill_in 'username', with: 'filterer'
     fill_in 'password', with: 'any password'
     click_button 'Login'
 
     # We should have all of our students present
-    assert page.has_content?("Student, Betty") 
-    assert page.has_content?("Student, Alvin") 
-    assert page.has_content?("Stone, Rolling") 
+    page.must_have_selector("#prospect_#{ students["Betty"].id  }") 
+    page.must_have_selector("#prospect_#{ students["Alvin"].id  }") 
+    page.must_have_selector("#prospect_#{ students["Rolling"].id  }") 
 
     find("#filter-prospects").click
-    page.must_have_selector( "#filter-modal", visible: true ) 
+    assert page.find( "#filter-modal" ).visible? 
   
     # we tweak the slider...
-    check("class_status_#{undergrad}") 
+    find("input#class_status_#{undergrad}").trigger(:click)
+    assert find("input#class_status_#{undergrad}")["checked"]
     
-    find("#submit-filter").click
+    find("#submit-filter").trigger(:click)
     page.must_have_selector( "#filter-modal", visible: false ) 
 
     # only alvin is an undergrad 
-    assert page.has_content?("Student, Alvin") 
-    refute page.has_content?("Student, Betty") 
-    refute page.has_content?("Stone, Rolling") 
+    page.wont_have_selector("#prospect_#{ students["Betty"].id  }") 
+    page.wont_have_selector("#prospect_#{ students["Rolling"].id  }") 
+    page.must_have_selector("#prospect_#{ students["Alvin"].id  }") 
   end
 end
