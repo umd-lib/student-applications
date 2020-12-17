be69ddae36dcd1e27643403efdba0cd3f08f1da5
David P. Steelman <dsteelma@umd.edu>
2020-12-17 08:36:22 -0500

    LIBITD-1802. Modified Docker start script to start delayed job daemon
    Modified the "rails_start.sh" script used by the "Dockerfile" to start
    the "delayed_job" daemon, so that an email is sent to the applicant
    when an application is submitted.
    
    https://issues.umd.edu/browse/LIBITD-1802

--- docker_config/student-applications/rails_start.sh
+++ docker_config/student-applications/rails_start.sh
@@ -3,6 +3,9 @@
 cd /home/app/webapp
 bundle exec rails db:migrate
 
+# Start the delayed job
+/home/app/webapp/bin/delayed_job start
+
 # Use "exec" to start Rails so that the application will receive the SIGTERM
 # sent to the root process (PID 1), giving the application a chance to
 # gracefully shutdown.
