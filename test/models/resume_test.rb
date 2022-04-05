41a4313b51aadefa42ff32520e977bb272b68ae0
David P. Steelman <dsteelma@umd.edu>
2022-04-05 13:54:11 -0400

    LIBITD-2113. Updated application to use ActiveStorage
    Active Storage does not have validation by default, so using the
    "active_storage_validations" gem.
    
    Updated models/controllers/views to use Active Storage, and also
    modified the unit tests and Rake tasks.
    
    https://issues.umd.edu/browse/LIBITD-2113

--- test/models/resume_test.rb
+++ test/models/resume_test.rb
@@ -7,14 +7,14 @@ class ResumeTest < ActiveSupport::TestCase
   test 'can create a valid resume' do
     resume = Resume.new
     assert_not resume.valid?
-    resume.file = File.new('test/fixtures/files/resume.pdf', 'r')
+    resume.file.attach(io: File.open('test/fixtures/files/resume.pdf'), filename: 'resume.pdf')
     assert resume.valid?
   end
 
   test 'it only allows pdfs' do
-    resume = Resume.new(file: File.new('test/fixtures/files/resume.pdf', 'r'))
+    resume = Resume.new(file: { io: File.open('test/fixtures/files/resume.pdf'), filename: 'resume.pdf' })
     assert resume.valid?
-    resume.file_content_type = 'bark/meow'
+    resume.file.content_type = 'bark/meow'
     assert_not resume.valid?
   end
 end
