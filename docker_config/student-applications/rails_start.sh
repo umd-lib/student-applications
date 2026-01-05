b1adef9c78249c09211102a7da832181e24e47ea
David P. Steelman <dsteelma@umd.edu>
2026-01-05 10:08:54 -0500

    LIBITD-2633. Modified Dockerfile to more closely match stock Rails 7.2.3
    Modified the "Dockerfile" to more closely match the "Dockerfile"
    generated by a stock Rails 7.2.3 installation.
    
    Among the changes:
    
    * The non-root user for the Rails application has been changed from
    "app" to "rails", and the user id/group id has been changed from
    "9999" to "1000"
    
    * The Rails application has been moved from the "/home/app/webapp"
    directory to the "/rails" directory.
    
    Updated the "rails_start.sh" script with the new application directory
    location, and removed the `bundle exec rails db:migrate` command,
    because database migration is taken care of by the "db:prepare" command
    in /rails/bin/docker-entrypoint, which is the ENTRYPOINT for the
    Docker container.
    
    https://umd-dit.atlassian.net/browse/LIBITD-2633

--- docker_config/student-applications/rails_start.sh
+++ docker_config/student-applications/rails_start.sh
@@ -1,10 +1,9 @@
 #!/usr/bin/env bash
 
-cd /home/app/webapp
-bundle exec rails db:migrate
+cd /rails
 
 # Start the delayed job
-/home/app/webapp/bin/delayed_job start
+/rails/bin/delayed_job start
 
 # Use "exec" to start Rails so that the application will receive the SIGTERM
 # sent to the root process (PID 1), giving the application a chance to
