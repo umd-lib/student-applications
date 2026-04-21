8ce9bcbb5f42e2e91482b6210e5d0ace47199c61
David P. Steelman <dsteelma@umd.edu>
2026-04-21 11:09:26 -0400

    LIBITD-2702. Adjusted local Rails "ci" process
    Rails 8.1 now adds a "ci" process that can run locally.
    
    Fixed minor issues with the "ci" process:
    
    * Made the "bin/bundler-audit" script executeable
    * In "config/bundle-audit.yml" replaced the "placeholder" CVE with
      an explanatory comment
    * Modified the "config/ci.rb" file to run the system tests
    
    https://umd-dit.atlassian.net/browse/LIBITD-2702

--- config/ci.rb
+++ config/ci.rb
@@ -10,7 +10,9 @@ CI.run do
   step "Tests: Seeds", "env RAILS_ENV=test bin/rails db:seed:replant"
 
   # Optional: Run system tests
-  # step "Tests: System", "bin/rails test:system"
+  # UMD Customization
+  step "Tests: System", "bin/rails test:system"
+  # End UMD Customization
 
   # Optional: set a green GitHub commit status to unblock PR merge.
   # Requires the `gh` CLI and `gh extension install basecamp/gh-signoff`.
